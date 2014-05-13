e066ff7759331fae06f6568f28480ed17760bf62
Painted <will.allen@paintedltd.co.uk>
2014-05-13 16:42:53 +0100

    created an interactive menu to add, view and exit

--- directory.rb
+++ directory.rb
@@ -1,3 +1,28 @@
+def interactive_menu
+  	students = []
+	loop do
+	# 1. print the menu and ask the user what to do
+	puts "1. Input the students"
+	puts "2. Show the students"
+	puts "9. Exit" #9 because we will add more options
+	# 2. read the input and save it into a variable
+	selection = gets.chomp
+	# 3. do what the user has asked
+	case  selection
+		when "1"
+		students = input_students
+		when "2"
+		print_header
+		print(students)
+		print_footer(students)
+		when "9"
+		exit# exit the program
+		else
+		puts "I don't know what you meant, try again"
+		end
+	end
+end
+
 #first we print the list of students
 def input_students
 	puts "Please enter the names of the students"
@@ -34,7 +59,4 @@ def print_footer(names)
 puts "Overall, we have #{names.length} great students"
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+interactive_menu
