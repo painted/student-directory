216f944a4b83fa4ea6ea78a4c2220807cf162311
Painted <will.allen@paintedltd.co.uk>
2014-05-13 15:33:49 +0100

    changed a hardcoded list to user input list

--- directory.rb
+++ directory.rb
@@ -1,25 +1,22 @@
 #first we print the list of students
-students = [
-{:name => "Roi Discoll", :cohort => :may},
-{:name => "James Whyte", :cohort => :may},
-{:name => "Margherita Serino", :cohort => :may},
-{:name => "Vaidas Mykolaitis", :cohort => :may},
-{:name => "Johann Bornman", :cohort => :may},
-{:name => "Kate Hamilton", :cohort => :may},
-{:name => "James Keap", :cohort => :may},
-{:name => "Nic Yeeles", :cohort => :may},
-{:name => "Julie Walker", :cohort => :may},
-{:name => "Will Allen", :cohort => :may},
-{:name => "Julia Tan", :cohort => :may},
-{:name => "Federico Maffei", :cohort => :may},
-{:name => "Jamie Patel", :cohort => :may},
-{:name => "Faezrah Rizalman", :cohort => :may},
-{:name => "Josh Fail-Broon", :cohort => :may},
-{:name => "Sasha Cooper", :cohort => :may},
-{:name => "Nicolai DTH", :cohort => :may},
-{:name => "Nadav Matalon", :cohort => :may},
-{:name => "Fitsum Teklehaimanot", :cohort => :may}
-]
+def input_students
+	puts "Please enter the names of the students"
+	puts "To finish, just hit return twice"
+	# create an empty array
+students = []
+	# get the first name
+	name = gets.chomp
+	# while the name is not empty, repeat this code
+	while !name.empty? do
+		#add the student hash to the array
+		students << {:name => name, :cohort => :may}
+		puts "Now we have #{students.length} students"
+		# get another name from the user
+		name = gets.chomp
+	end
+	#return the array of students
+	students
+end
 
 def print_header
 puts "The students of my cohort at Makers Academy"
@@ -37,6 +34,7 @@ def print_footer(names)
 puts "Overall, we have #{names.length} great students"
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
